b6168914302428b5c9215a4d9dbfd8b3242e21b1
AdrieneTZ <tznali17@gmail.com>
2022-06-20 16:55:56 +0800

    style: style register and login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -6,9 +6,13 @@ router.get('/register', (req, res) => {
   return res.render('register')
 })
 
+// route: POST/ users/ register
+
 // route: GET/ users/ login
 router.get('/login', (req, res) => {
   return res.render('login')
 })
+
+// route: POST/ users/ login
 
 module.exports = router
