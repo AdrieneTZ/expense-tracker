c81cc554cdb4a2cd06c7637d03b51a3b39d17e8d
AdrieneTZ <tznali17@gmail.com>
2022-06-20 20:17:07 +0800

    feat: add error message for each type of error in POST register route

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -10,26 +10,42 @@ router.get('/register', (req, res) => {
 })
 
 // route: POST/ users/ register
-router.post('/register', (req, res) => {
   // step 1. get the form data
-  const { name, email, password, confirmPassword } = req.body
-
   // step 2. check if the user already exists. condition: email
   // step 3. if exist, return to the register page with the data user entered
-  // step 4. if not exist, create a new user ( add id ) and direct to the home page
-User.findOne({ email })
-  .then(user => {
-    if (user) {
-      console.log('This email is already registered.')
-      return res.render('register', { name, email, password, confirmPassword })
-    }
-    User.create({ name, email, password })
-  })
-  .then(() => res.redirect('/'))
-  .catch(error => console.log(error))
-
+  // step 4. if not exist, create a new user and direct to the home page
   // step 5. give each type of error message
   // step 6. add bcrypt to hash the password before saving it to the database
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: 'All fields are required!' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: 'Password and Confirm Password do not match!' })
+  }
+  if(errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
+
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        errors.push({ message: 'This email has been registered!' })
+        return res.render('register', { errors, name, email, password, confirmPassword })
+      }
+      User.create({ name, email, password })
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
+    })
 })
 
 // route: GET/ users/ login
