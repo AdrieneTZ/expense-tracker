ee83cb4e33712dda497f0e863284dcebcb03a790
AdrieneTZ <tznali17@gmail.com>
2022-06-26 08:09:57 +0800

    feat: add feature: edit a record

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 const flash = require('connect-flash')
 
 if (process.env.NODE_ENV !== 'production') {
@@ -28,6 +29,7 @@ app.use(session({
 }))
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 usePassport(app)
 app.use(flash())
--- routes/modules/records.js
+++ routes/modules/records.js
@@ -30,4 +30,38 @@ router.post('/', async (req, res) => {
   }
 })
 
+// router: GET/ records/ :recordId/ edit
+router.get('/:id/edit', async (req, res) => {
+  try {
+    const userId = req.user._id
+    const _id = req.params.id
+    const record = await Record.findOne({ userId, _id }).lean()
+    const categoryOfRecord = await Category.findById(record.categoryId).lean()
+    record.category = categoryOfRecord.name
+
+    const categories = await Category.find().lean()
+
+    res.render('edit', { record, categories })
+  } catch(error) {
+    console.log('Fail to get the page of editting this expense record', error)
+    res.send(`<p4>Fail to get the page of editting this expense record</p4>`)
+  }
+})
+
+// router: PUT/ records/ :recordId/ edit
+router.put('/:id', async (req, res) => {
+  try {
+    const userId = req.user._id
+    const _id = req.params.id
+    const { name, date, amount, categoryId } = req.body
+
+    await Record.findByIdAndUpdate(_id, { name, date, amount: Number(amount), categoryId, userId })
+
+    res.redirect('/')
+  } catch(error) {
+    console.log('Fail to save this editted expense record', error)
+    res.send(`<p4>Fail to save this editted expense record</p4>`)
+  }
+})
+
 module.exports = router
